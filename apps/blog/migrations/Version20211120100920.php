73319f8e15adf1464a2f4f3da5c282c073d514df
Lorthioir <jeremy.lorthioir24@gmail.com>
2021-11-21 16:54:56 +0100

    [Improve] More migrations

---
+++ apps/blog/migrations/Version20211120100920.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211120100920 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category DROP FOREIGN KEY fk_category_parent');
+        $this->addSql('DROP INDEX idx_category_parent ON category');
+        $this->addSql('ALTER TABLE category DROP parentId');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category ADD parentId INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT fk_category_parent FOREIGN KEY (parentId) REFERENCES category (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX idx_category_parent ON category (parentId)');
+    }
+}
